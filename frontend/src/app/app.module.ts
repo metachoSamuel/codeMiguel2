01a36c0af0e96b9ab9126f04aa72d943a0955e2b
CarolinaRoa <90359261+CarolinaRoa@users.noreply.github.com>
2021-11-02 20:09:11 -0500

    pagina inicio

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,46 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+// Librería para poder consumir el servicio
+import { HttpModule, } from '@angular/http';
+import { HttpClientModule, } from '@angular/common/http';
+import { RouterModule, Routes} from '@angular/router';
+import { FormsModule, ReactiveFormsModule  } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './appcomponent/app.component';
+import { MenuInicioComponent } from './menu-inicio/menu-inicio.component';
+import { ServicioService } from './servicio.service';
+
+const appRoutes: Routes = 
+[
+  {
+    path: '',
+    pathMatch: 'prefix',
+    redirectTo: 'Inicio'
+  },
+  {
+    path: 'Inicio',
+    component:MenuInicioComponent,
+  }
+]
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenuInicioComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpModule,
+    RouterModule.forRoot(appRoutes), // se agregan estos 
+    BrowserModule,
+    HttpClientModule  // <- Agregar la clase
+  ],
+  providers: [ServicioService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
